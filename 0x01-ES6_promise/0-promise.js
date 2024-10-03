f056420e50933ea1f8c472c8cb2ad38e661fb60f
Brehyma Traore <brehymatraore50@gmail.com>
2024-10-03 15:36:18 +0000

    Return the status

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,7 @@
 export function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        // Simulate an asynchronous operation (e.g., API call)
-    });
+  return new Promise((resolve, reject) => {
+    // Simulate an asynchronous operation (e.g., API call)
+  });
 }
 
 export default getResponseFromAPI;
