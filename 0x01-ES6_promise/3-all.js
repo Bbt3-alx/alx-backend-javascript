8621e62b1f5d85b36f5da0cd702efc76cadfd9aa
Brehyma Traore <brehymatraore50@gmail.com>
2024-10-03 21:45:08 +0000

    converte the function into async

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,12 @@
 import { uploadPhoto, createUser } from './utils';
 
-function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then((values) => {
-      console.log(values[0].body, values[1].firstName, values[1].lastName);
-    })
-    .catch(() => {
-      console.error('Signup system offline');
-    });
+async function handleProfileSignup() {
+  try {
+    const values = await Promise.all([uploadPhoto(), createUser()]);
+    console.log(values[0].body, values[1].firstName, values[1].lastName);
+  } catch (e) {
+    console.error('Signup system offline');
+  }
 }
 
 export default handleProfileSignup;
