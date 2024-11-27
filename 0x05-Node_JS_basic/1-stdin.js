45ad40c1927693267f214bbd85b640feb5a78e7d
Brehyma Traore <brehymatraore50@gmail.com>
2024-11-27 10:58:07 +0000

    End-Of-File handled

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,13 @@
 process.stdin.setEncoding('utf-8');
 
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', function(data) {
   const name = data.trim();
-    process.stdout.write(`Your name is: ${name}\n`);
-    process.exit()
+  console.log(`Your name is: ${name}`);
 });
 
 process.stdin.on('end', function() {
-  process.stdout.write('This important software is now closing\n');
+  console.log('This important software is now closing');
   process.exit();
 });
 
