173e89cebd98077a0374f0b1ab3b511882dc242c
Brehyma Traore <brehymatraore50@gmail.com>
2024-11-27 02:59:00 +0000

    Update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,14 @@
 process.stdin.setEncoding('utf-8');
 
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('readable', function() {
-  var chunk = process.stdin.read();
-  if (chunk !== null) {
-    process.stdout.write(`Your name is: ${chunk}\n`);
-  }
+process.stdin.on('data', function(data) {
+  const name = data.trim();
+    process.stdout.write(`Your name is: ${name}\n`);
+    process.exit()
 });
 
 process.stdin.on('end', function() {
   process.stdout.write('This important software is now closing\n');
+  process.exit();
 });
 
