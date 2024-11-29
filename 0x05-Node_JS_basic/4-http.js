4d23eb2bd35d1459d097dea427ffb4b2697e47e8
Brehyma Traore <brehymatraore50@gmail.com>
2024-11-29 23:32:55 +0000

    Updated

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,7 +1,9 @@
 const http = require('http');
-const server = http.createServer( (req, res) => {
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.write('Hello Holberton School!');
-  res.end()
+  res.end();
 });
 
 server.listen(1245);
